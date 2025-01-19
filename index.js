4bdd44cfd57fad620fa5b33d37d9cac3f3a62eb2
ab-rahman <abr03348@gmail.com>
2025-01-19 14:31:07 +0600

    updated coins with task added

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
 const morgan = require("morgan");
-// const jwt = require("jsonwebtoken");
+const jwt = require("jsonwebtoken");
 
 // const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
 const { MongoClient, ServerApiVersion } = require("mongodb");
@@ -10,7 +10,11 @@ const app = express();
 const port = process.env.PORT || 5000;
 
 // middleware
-app.use(cors());
+app.use(
+  cors({
+    origin: ["http://localhost:5173"],
+  })
+);
 app.use(express.json());
 app.use(morgan("dev"));
 
@@ -32,8 +36,66 @@ async function run() {
 
     const db = client.db("quickTasker");
     const userCollection = db.collection("users");
+    const taskCollection = db.collection("tasks");
 
-    // users related APIs
+    // JWT------------------------------------
+    app.post("/jwt", async (req, res) => {
+      const user = req.body;
+      const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
+        expiresIn: "5h",
+      });
+      res.send(token);
+    });
+
+    // middleware
+    const verifyToken = (req, res, next) => {
+      if (!req.headers.authorization) {
+        return res.status(401).send({ message: "Unauthorized access" });
+      }
+
+      const token = req.headers.authorization.split(" ")[1];
+      jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+        if (err) {
+          return res.status(401).send({ message: "Unauthorized access" });
+        }
+
+        req.decoded = decoded;
+        next();
+      });
+    };
+
+    // const verifyRole = (requiredRole) => {
+    //   return async (req, res, next) => {
+    //     const email = req.decoded.email;
+    //     const user = await userCollection.findOne({ email });
+    //     if (user?.role !== requiredRole) {
+    //       return res.status(403).send({ message: "Forbidden access" });
+    //     }
+    //     next();
+    //   };
+    // };
+
+    // const verifyAdmin = async (req, res, next) => {
+    //   const email = req.decoded.email;
+    //   const filter = { email };
+    //   const user = await userCollection.findOne(filter);
+    //   const isAdmin = user?.role === "admin";
+
+    //   if (!isAdmin) {
+    //     return res.status(403).send({ message: "Forbidden access" });
+    //   }
+    //   next();
+    // };
+
+    // users related APIs----------------------------------------------
+
+    app.get("/users/coins/:email", async (req, res) => {
+      const email = req.params.email;
+      const filter = { email };
+      const user = await userCollection.findOne(filter);
+      res.send({ coins: user?.availableCoins });
+    });
+
     app.post("/users", async (req, res) => {
       const userInfo = req.body;
       const email = userInfo.email;
@@ -44,6 +106,43 @@ async function run() {
       }
       const result = await userCollection.insertOne(userInfo);
       res.send(result);
+    });
+
+    // getting role
+    app.get("/users/role/:email", verifyToken, async (req, res) => {
+      const email = req.params.email;
+
+      if (email !== req.decoded.email) {
+        return res.status(403).send({ message: "Forbidden access" });
+      }
+
+      const filter = { email };
+      const user = await userCollection.findOne(filter);
+
+      res.send({ role: user?.role });
+    });
+
+    // Tasks related --------------------------------------------
+    app.post("/task", async (req, res) => {
+      const taskData = req.body;
+      const { email, required_workers, payable_amount } = taskData;
+      const taskAdded = await taskCollection.insertOne(taskData);
+      if (taskAdded.insertedId) {
+        const coinsUpdatedResult = await userCollection.updateOne(
+          {
+            email: email,
+          },
+          {
+            $inc: {
+              availableCoins: -required_workers * payable_amount,
+            },
+          }
+        );
+        if (coinsUpdatedResult.modifiedCount > 0) {
+          return res.send({ message: "Coins Updated successfully!" });
+        }
+      }
+      res.send(taskAdded);
     });
 
     // Send a ping to confirm a successful connection
