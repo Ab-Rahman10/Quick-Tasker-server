19531006381fd34a6f32fff0516d96e19d6f9eb2
ab-rahman <abr03348@gmail.com>
2025-01-26 00:13:13 +0600

    added stripe based payment func

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const cors = require("cors");
 const morgan = require("morgan");
 const jwt = require("jsonwebtoken");
 
-// const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
+const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const app = express();
 const port = process.env.PORT || 5000;
@@ -38,6 +38,7 @@ async function run() {
     const userCollection = db.collection("users");
     const taskCollection = db.collection("tasks");
     const submissionCollection = db.collection("submissions");
+    const orderCollection = db.collection("orders");
 
     // JWT------------------------------------
     app.post("/jwt", async (req, res) => {
@@ -291,6 +292,17 @@ async function run() {
       res.send(pendingAllTasks);
     });
 
+    // get total paid payment by buyer
+    app.get("/total-paid-payment/:email", verifyToken, async (req, res) => {
+      const user = req.params.email;
+      const filter = { buyer_email: user };
+      const allSubmissions = await submissionCollection.find(filter).toArray();
+      const approveSubs = allSubmissions.filter(
+        (approve) => approve.status === "Approved"
+      );
+      res.send(approveSubs);
+    });
+
     // after clicking approve button
     app.patch("/approve-task", verifyToken, async (req, res) => {
       const data = req.body;
@@ -380,6 +392,55 @@ async function run() {
     app.post("/submission", verifyToken, async (req, res) => {
       const submissionData = req.body;
       const result = await submissionCollection.insertOne(submissionData);
+      res.send(result);
+    });
+
+    // Create payment intent
+    app.post("/create-payment-intent", verifyToken, async (req, res) => {
+      const { price } = req.body;
+      const { client_secret } = await stripe.paymentIntents.create({
+        amount: price * 100,
+        currency: "usd",
+        automatic_payment_methods: {
+          enabled: true,
+        },
+      });
+
+      res.send({ clientSecret: client_secret });
+    });
+
+    // Order related APIs---------------------------------------------------------
+
+    app.get("/orders/:email", verifyToken, async (req, res) => {
+      const email = req.params.email;
+      const filter = { "user.email": email };
+      const result = await orderCollection.find(filter).toArray();
+      res.send(result);
+    });
+
+    app.post("/order", verifyToken, async (req, res) => {
+      const orderData = req.body;
+      const result = await orderCollection.insertOne(orderData);
+      res.send(result);
+    });
+
+    // Update coins after payment
+    app.patch("/add-payment-coin/:email", verifyToken, async (req, res) => {
+      // get payment coins
+      const { orderId } = req.body;
+      const query = { _id: new ObjectId(orderId) };
+      const getCoins = await orderCollection.findOne(query);
+
+      // increase coins
+      const buyerEmail = req.params.email;
+      const filter = { email: buyerEmail };
+      const updateDoc = {
+        $inc: {
+          availableCoins: getCoins?.purchase?.coins,
+        },
+      };
+
+      const result = await userCollection.updateOne(filter, updateDoc);
       res.send(result);
     });
 
