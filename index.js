a96cf44a1c1175001342bd5fcbb35a2a0fbcde6c
ab-rahman <abr03348@gmail.com>
2025-01-27 12:37:41 +0600

    added firebase URL

--- index.js
+++ index.js
@@ -12,7 +12,11 @@ const port = process.env.PORT || 5000;
 // middleware
 app.use(
   cors({
-    origin: ["http://localhost:5173"],
+    origin: [
+      "http://localhost:5173",
+      "https://quick-tasker-d56fa.firebaseapp.com",
+      "https://quick-tasker-d56fa.web.app",
+    ],
   })
 );
 app.use(express.json());
